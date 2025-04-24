1175e1ffcc7207f6a40b87cedc753e99797dbb1a
RevuLakshman <lakshman.revu@gmail.com>
2025-04-24 11:53:39 +0530

    Added uplod, deploy and request services to git.

---
+++ upload_service/src/aws.ts
@@ -0,0 +1,20 @@
+import { S3 } from "aws-sdk";
+import fs from "fs";
+
+const s3 = new S3({
+    accessKeyId: "",
+    secretAccessKey: "",
+    endpoint: ""
+})
+
+// fileName => output/12312/src/App.jsx
+// filePath => /Users/harkiratsingh/vercel/dist/output/12312/src/App.jsx
+export const uploadFile = async (fileName: string, localFilePath: string) => {
+    const fileContent = fs.readFileSync(localFilePath);
+    const response = await s3.upload({
+        Body: fileContent,
+        Bucket: "vercel",
+        Key: fileName,
+    }).promise();
+    console.log(response);
+}
